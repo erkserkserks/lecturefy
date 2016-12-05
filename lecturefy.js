119b886b43e8544606ea871986198ffbbf69491d
Eric Thong <thongerick@gmail.com>
2016-12-04 21:21:11 -0500

    Update to work with SCPD player as of 12/2016

--- lecturefy.js
+++ lecturefy.js
@@ -1,10 +1,10 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2014 erkserkserks
+ * Copyright (c) 2016 erkserkserks
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
+ * of this software and associated documentation files (the 'Software'), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
@@ -13,7 +13,7 @@
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -22,61 +22,105 @@
  * SOFTWARE.
  */
 
-(function() {
+(function(){
+  'use strict';
+
+  function getURLs() {
+    function createStatus() {
+      // Create new div for status information
+      $('.page-header').after('<p id=\'lfy-status\'> </div>');
+    }
+
+    function updateStatus(current, target) {
+      console.log('Fetching link ' + current + ' out of ' + target + '...');
+      $('#lfy-status').html('<h2> Fetching link ' + current + ' out of ' + target + '...</h2>');
+    }
+
+    function doneStatus(target) {
+      console.log('Done fetching ' + target + ' links!');
+      $('#lfy-status').html('<h2> Done fetching ' + target + ' links!</h2>');
+    }
+
+    createStatus();
+    var buttons = $('.watchVideoButton');
+    var lowQualityURLs = [];
+    var highQualityURLs = [];
+    var fetched = 0;
+
+    // For each lecture, get the video URLs
+    for (var i = 0; i < buttons.length; i++) {
+      var button = $(buttons[i]);
+      var lectureId = button.attr('data-lecture-id');
+      var courseId = button.attr('data-course-id');
+      var playerType = button.attr('data-type');
+      $.ajax({
+        url: '/' + playerType + '/LoadPlayer',
+        type: 'get',
+        data: {
+          lectureId: lectureId,
+          courseId: courseId
+        },
+        success: function (result) {
+          if (result.length > 0) {
+            var re = /file: "(.*?)"/g;
+            // TODO(ethong): Error handling in case we don't find what we expect
+            lowQualityURLs.push(re.exec(result)[1]);
+            highQualityURLs.push(re.exec(result)[1]);
+            fetched++;
+            var target = buttons.length;
+            updateStatus(fetched, target);
+            if (fetched == target) {
+              doneStatus(target);
+              displayURLs(lowQualityURLs, highQualityURLs);
+            }
+          }
+        }
+      });
+    }
+  }
+
+  function displayURLs(lowQualityURLs, highQualityURLs) {
+    // Print URLs from array, one on each line
+    function printURLs(array) {
+      for (var i = 0; i < array.length; i++) {
+        console.log(array[i]);
+        $('#lfy-urls').append('<br><a href=\'' + array[i] + '\'>' + array[i] + '</a></br>');
+      }
+    }
+
+    // Create new div for output
+    $('#lfy-status').after('<div id=\'lfy-urls\'> </div>');
+
+    // Output low quality links
+    console.log('LQ Links: ');
+    $('#lfy-urls').append('<h2> Low quality video links (smaller files): </h2>');
+    printURLs(lowQualityURLs);
+    $('#lfy-urls').append('<br></br>');
+
+    // Output high quality links
+    console.log('HQ Links: ');
+    $('#lfy-urls').append('<h2> High quality video links (larger files): </h2>');
+    printURLs(highQualityURLs);
+  }
+
   // Some sanity checks
-  if (window.location.host.indexOf("mvideox") !== 0) {
-    alert('Invalid domain name!\n' + 
+  if (window.location.host.indexOf('mvideox') !== 0) {
+    alert('Invalid domain name!\n' +
           'Go to mvideox.stanford.edu');
     return;
   }
 
-  if ($('#myVideoContainer').length === 0) {
-    alert('No video player detected!\n' +
-          'Select a class, click on any lecture video and try again.');
+  if ($('#readyToWatch').length === 0) {
+    alert('No class selected!\n' +
+          'Select a class, and try again.');
     return;
   }
 
-  if ($('#urls-output').length !== 0) {
-    console.log('#urls-output already exists! Quitting.');
+  if ($('#lfy-urls').length !== 0) {
+    console.log('#lfy-urls already exists! Quitting.');
     return;
   }
 
-  // Grab course detail info from SCPD's Angular.js app
-  var cd = angular.element('#myVideoContainer').scope().courseDetail;
-  var weeks = cd.CourseWeek;
-
-  // Grab video URLs
-  var highQualityURLs = [];
-  var lowQualityURLs = [];
-
-  for (var i = 0; i < weeks.length; i++) {
-    var lectures = weeks[i].Lectures;
-    for (var j = 0; j < lectures.length; j++) {
-      var lecture = lectures[j];
-      highQualityURLs.push(lecture.HiVideoDownloadUrl);
-      lowQualityURLs.push(lecture.LowVideoDownloadUrl);
-    }
-  }
-
-  // Print URLs from array, one on each line
-  function printURLs(array) {
-    for (var i = 0; i < array.length; i++) {
-      console.log(array[i]);
-      $('#urls-output').append('<br><a href="' + array[i] + '">' + array[i] + '</a></br>');
-    }
-  }
-
-  // Create new div for output
-  $('.page-header').after('<div id="urls-output"> </div>');
-
-  // Output high quality links
-  console.log('HQ Links: ');
-  $('#urls-output').append('<h2> High quality video links (larger files): </h2>');
-  printURLs(highQualityURLs);
-  $('#urls-output').append('<br></br>');
-
-  // Output low quality links
-  console.log('LQ Links: ');
-  $('#urls-output').append('<h2> Low quality video links (smaller files): </h2>');
-  printURLs(lowQualityURLs);
+  // Start getting URLs!
+  getURLs();
 })();
